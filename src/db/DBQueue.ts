2c1e550bf5f22e9938ba19c59442c7d497c95022
CandidoRPNeto <candido.neto@ucsal.edu.br>
2024-12-29 17:17:30 -0300

    add messages

--- src/db/DBQueue.ts
+++ src/db/DBQueue.ts
@@ -1,5 +1,6 @@
 import { Pool, QueryResult } from "pg";
 import { DataFile, MessageFile } from "../files_object/File";
+import { Messages } from "../tools/Messages";
 export interface PGConnectInfo{
     host: string;
     port: number;
@@ -10,43 +11,59 @@ export interface PGConnectInfo{
 
 export class PgDataBase {
     private pool!: Pool;
-    private enableTransaction: boolean;
 
-    constructor(connect: PGConnectInfo, enableTransaction: boolean = false) {
+    constructor(connect: PGConnectInfo) {
         this.pool = new Pool(connect);
-        this.enableTransaction = enableTransaction;
     }
     
     public async rawQuerySelect(finalTableName: string, query: string, values: [] = []): Promise<DataFile> {
-        Messages.new("RAW QUERY","unkown").loading();
+        const message = Messages.new("RAW QUERY","unkown");
         try {
+            message.loading();
             let data = await this.query(query, values);
+            message.success();
             return new DataFile(finalTableName, data.rows);
-        } catch (error) { return new DataFile(finalTableName, {message: "Fail to run", error},"data/logs"); }
+        } catch (error) { 
+            message.fail();
+            return new DataFile(finalTableName, {message: "Fail to run", error},"data/logs"); 
+        }
     }
 
     public async insert(table: string, object: {}) {
         const insert = this.buildInsert(table, object);
-        Messages.new("INSERT",table).loading();
+        const message = Messages.new("INSERT",table);
         try {
+            message.loading();
             let data = await this.query(insert.query, insert.values);
+            message.success();
             return new DataFile(table, data.rows,"inserts");   
-        } catch (error) { return new DataFile(table, {message: "Fail to insert", object, error},"inserts/logs"); }
+        } catch (error) { 
+            message.fail();
+            return new DataFile(table, {message: "Fail to insert", object, error},"inserts/logs"); 
+        }
     }
 
     public async manyInserts(table: string, objects: {}[]) {
         const client = await this.pool.connect();
         const result: any[] = [];
-        Messages.new("MANY INSERTS",table).loading();
+        const message = Messages.new("MANY INSERTS",table);
         let lastObj:any;
         try {
+            message.loading();
+            await client.query('BEGIN');
             for (const obj of objects) {
                 lastObj = obj;
                 const insert = this.buildInsert(table, obj);
                 const response = await (await client.query(insert.query, insert.values)).rows;
                 result.push(response);
             }
-        } catch (error) { new DataFile(table, {message: "Fail to insert", lastObj, error},"inserts/logs"); }       
+            await client.query('COMMIT');
+        } catch (error) {
+            await client.query('ROLLBACK');
+            message.fail();
+            new DataFile(table, {message: "Fail to insert", lastObj, error},"inserts/logs"); 
+        }
+        message.success();
         await client.release();
         return new DataFile(table, result,"inserts");
     }
@@ -60,11 +77,16 @@ export class PgDataBase {
     
     public async select(table: string, wheres: {}, fields: [] = []): Promise<DataFile> {
         let values = Object.values(wheres)? Object.values(wheres) : [];
-        Messages.new("SELECT",table).loading();
+        const message = Messages.new("SELECT",table);
+        message.loading();
         try {
             let data = await this.query(this.buildSelect(table, Object.keys(wheres), fields), values);
+            message.success();
             return new DataFile(table, data.rows);
-        } catch (error) { return new DataFile(table, {message: "Fail to select", error},"inserts/logs"); }   
+        } catch (error) { 
+            message.fail();
+            return new DataFile(table, {message: "Fail to select", error},"inserts/logs"); 
+        }   
     }
 
     public async update(table: string, values: {}, wheres: {}) {
@@ -76,13 +98,18 @@ export class PgDataBase {
         for (let i = 1; i < wheresKeys.length; i++) { query = `${query} AND ${wheresKeys[i]} = $${i + setKeys.length + 1}`; }
         const data = Object.values(values).concat(Object.values(wheres));
         const SELECTQUERY = this.buildSelect(table, wheresKeys);
-        Messages.new("UPDATE",table).loading();
+        const message = Messages.new("INSERT",table);
+        message.loading();
         try {
             let oldOnes = await this.query(SELECTQUERY, Object.values(wheres));
             await this.query(query, data)
             let newOnes = await this.query(SELECTQUERY, Object.values(wheres));
+            message.success();
             return new DataFile(table, {new: newOnes.rows, old: oldOnes.rows},"updates");
-        } catch (error) { return new DataFile(table, {message: "Fail to update", query, wheres, values, error},"updates/logs"); }
+        } catch (error) {
+            message.fail();
+            return new DataFile(table, {message: "Fail to update", query, wheres, values, error},"updates/logs"); 
+        }
     }
 
     public async delete(table: string, wheres: {}) {
@@ -90,12 +117,17 @@ export class PgDataBase {
         const values = Object.values(wheres);
         let query = `DELETE FROM ${table} WHERE ${keys[0]} = $${1}`;
         for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
-        Messages.new("DELETE",table).loading();
+        const message = Messages.new("INSERT",table);
+        message.loading();
         try {
             let result = await this.query(this.buildSelect(table, keys), values);
-            await this.query(query, values)
+            await this.query(query, values);
+            message.success();
             return new DataFile(table, result.rows,"deletes");
-        } catch (error) { return new DataFile(table, {message: "Fail to delete", wheres, query, error},"deletes/logs"); }
+        } catch (error) { 
+            message.fail();
+            return new DataFile(table, {message: "Fail to delete", wheres, query, error},"deletes/logs"); 
+        }
     }
 
     private buildSelect(table: string, keys: string[], fields: [] = []):string {
@@ -112,30 +144,5 @@ export class PgDataBase {
         catch (error) { result =  { oid: 0, rows: [], fields: [], command: '', rowCount: 0 }; }
         await client.release();
         return result;
-    }
-}
-export class Messages {
-    phase: string | undefined;
-    table: string | undefined;
-
-    constructor(phase: string,table: string){
-        this.phase = phase;
-        this.table = table;
-    }
-
-    static new(phase: string,table: string){
-        return new Messages(phase, table);
-    }
-
-    public loading() {
-        console.log(`iniciando a fase de ${this.phase} na tabela ${this.table}`);
-    }
-
-    public fail() {
-        
-    }
-
-    public success() {
-        
     }
 }
