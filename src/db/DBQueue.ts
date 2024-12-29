bd6008beee99035b18ae9637ddfe67218ad87265
CandidoRPNeto <candido.neto@ucsal.edu.br>
2024-12-29 04:25:58 -0300

    fix all crud basic uses and add log

--- src/db/DBQueue.ts
+++ src/db/DBQueue.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Pool, QueryResult } from "pg";
 import { DataFile, MessageFile } from "../files_object/File";
 export interface PGConnectInfo{
     host: string;
@@ -18,37 +18,37 @@ export class PgDataBase {
     }
     
     public async rawQuerySelect(finalTableName: string, query: string, values: [] = []): Promise<DataFile> {
-        let data = await this.query(query, values);
-        return new DataFile(finalTableName, data.rows);
-    }
-    
-    public async select(table: string, wheres: {}, fields: [] = []): Promise<DataFile> {
-        let columns = fields.length > 0 ? fields.join(",") : "*";
-        let keys = Object.keys(wheres);
-        let query = `SELECT ${columns} FROM ${table} WHERE ${keys[0]} = $${1}`;
-        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
-        let values = Object.values(wheres)? Object.values(wheres) : [];
-        let data = await this.query(query, values);
-        return new DataFile(table, data.rows);
+        Messages.new("RAW QUERY","unkown").loading();
+        try {
+            let data = await this.query(query, values);
+            return new DataFile(finalTableName, data.rows);
+        } catch (error) { return new DataFile(finalTableName, {message: "Fail to run", error},"data/logs"); }
     }
 
     public async insert(table: string, object: {}) {
         const insert = this.buildInsert(table, object);
-        console.log(insert);
-        let data = await this.query(insert.query, insert.values);
-        return new DataFile(table, data.rows);
+        Messages.new("INSERT",table).loading();
+        try {
+            let data = await this.query(insert.query, insert.values);
+            return new DataFile(table, data.rows,"inserts");   
+        } catch (error) { return new DataFile(table, {message: "Fail to insert", object, error},"inserts/logs"); }
     }
 
     public async manyInserts(table: string, objects: {}[]) {
         const client = await this.pool.connect();
         const result: any[] = [];
-        for (const obj of objects) {
-            const insert = this.buildInsert(table, obj);
-            const response = await (await client.query(insert.query, insert.values)).rows;
-            result.push(response);
-        }
+        Messages.new("MANY INSERTS",table).loading();
+        let lastObj:any;
+        try {
+            for (const obj of objects) {
+                lastObj = obj;
+                const insert = this.buildInsert(table, obj);
+                const response = await (await client.query(insert.query, insert.values)).rows;
+                result.push(response);
+            }
+        } catch (error) { new DataFile(table, {message: "Fail to insert", lastObj, error},"inserts/logs"); }       
         await client.release();
-        return new DataFile(table, result);
+        return new DataFile(table, result,"inserts");
     }
 
     private buildInsert(table: string, object: {}) {
@@ -56,6 +56,15 @@ export class PgDataBase {
         const values = Object.values(object);
         let query = `INSERT INTO ${table} (${columns}) VALUES (${values.map((_, i) => `$${i + 1}`).join(", ")})`;
         return {query: `${query} RETURNING *`, values: values};
+    }
+    
+    public async select(table: string, wheres: {}, fields: [] = []): Promise<DataFile> {
+        let values = Object.values(wheres)? Object.values(wheres) : [];
+        Messages.new("SELECT",table).loading();
+        try {
+            let data = await this.query(this.buildSelect(table, Object.keys(wheres), fields), values);
+            return new DataFile(table, data.rows);
+        } catch (error) { return new DataFile(table, {message: "Fail to select", error},"inserts/logs"); }   
     }
 
     public async update(table: string, values: {}, wheres: {}) {
@@ -66,23 +75,67 @@ export class PgDataBase {
         query = `${query.slice(0, -1)} WHERE ${wheresKeys[0]} = $${setKeys.length + 1}`;
         for (let i = 1; i < wheresKeys.length; i++) { query = `${query} AND ${wheresKeys[i]} = $${i + setKeys.length + 1}`; }
         const data = Object.values(values).concat(Object.values(wheres));
-        let result = await this.query(query, data);
-        return new DataFile(table, result.rows);
+        const SELECTQUERY = this.buildSelect(table, wheresKeys);
+        Messages.new("UPDATE",table).loading();
+        try {
+            let oldOnes = await this.query(SELECTQUERY, Object.values(wheres));
+            await this.query(query, data)
+            let newOnes = await this.query(SELECTQUERY, Object.values(wheres));
+            return new DataFile(table, {new: newOnes.rows, old: oldOnes.rows},"updates");
+        } catch (error) { return new DataFile(table, {message: "Fail to update", query, wheres, values, error},"updates/logs"); }
     }
 
     public async delete(table: string, wheres: {}) {
         let keys = Object.keys(wheres);
         const values = Object.values(wheres);
-        let query = `DELETE FROM ${table} WHERE ${keys[0]} = $${values.length + 1}`;
-        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + values.length + 1}`; }
-        let result = await this.query(query, values);
-        return new DataFile(table, result.rows);
+        let query = `DELETE FROM ${table} WHERE ${keys[0]} = $${1}`;
+        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
+        Messages.new("DELETE",table).loading();
+        try {
+            let result = await this.query(this.buildSelect(table, keys), values);
+            await this.query(query, values)
+            return new DataFile(table, result.rows,"deletes");
+        } catch (error) { return new DataFile(table, {message: "Fail to delete", wheres, query, error},"deletes/logs"); }
+    }
+
+    private buildSelect(table: string, keys: string[], fields: [] = []):string {
+        let columns = fields.length > 0 ? fields.join(",") : "*";
+        let query = `SELECT ${columns} FROM ${table} WHERE ${keys[0]} = $${1}`;
+        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
+        return query;
     }
     
     public async query(query: string, data: unknown[] = []) {
         const client = await this.pool.connect();
-        let result = await client.query(query, data);
+        let result:QueryResult<any>;
+        try { result = await client.query(query, data); } 
+        catch (error) { result =  { oid: 0, rows: [], fields: [], command: '', rowCount: 0 }; }
         await client.release();
         return result;
+    }
+}
+export class Messages {
+    phase: string | undefined;
+    table: string | undefined;
+
+    constructor(phase: string,table: string){
+        this.phase = phase;
+        this.table = table;
+    }
+
+    static new(phase: string,table: string){
+        return new Messages(phase, table);
+    }
+
+    public loading() {
+        console.log(`iniciando a fase de ${this.phase} na tabela ${this.table}`);
+    }
+
+    public fail() {
+        
+    }
+
+    public success() {
+        
     }
 }
--- src/files_object/File.ts
+++ src/files_object/File.ts
@@ -5,6 +5,7 @@ interface Archive {
     currentFileId: string;
     name:string;
     path: string;
+    position:number;
 }
 
 interface LogArchive extends Archive {
@@ -15,22 +16,22 @@ interface LogArchive extends Archive {
 
 class Archive implements Archive {
     path: string;
+    static position = 1;
     
     constructor() {
-        this.currentFileId = getTimeInfo();
+        this.currentFileId = `${getTimeInfo()}-${Archive.position}`;
         this.path = "./files";
+        Archive.position += 1;
     }
 }
 
 export class DataFile extends Archive {
-    currentFileId: string = "";
-    constructor(name:string, data: any[]) {
+    constructor(name:string, data: any, type:string = "data") {
         super();
-        this.name = name;
-        this.path = `${this.path}/${this.name}/data`;
-        const dirPath = path.dirname(this.path);
-        if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true });
-        fs.writeFileSync(`${this.path}/${this.currentFileId}.json`, JSON.stringify(data, null, 2));
+        this.name = name.replace(".","-");
+        this.path = `${this.path}/${this.name}/${type}/`;
+        if (!fs.existsSync(this.path)) fs.mkdirSync(this.path, { recursive: true });
+        fs.writeFileSync(`${this.path}${this.currentFileId}.json`, JSON.stringify(data, null, 2));
     }
 
     public getCopyOfCurrentFileContent(){
@@ -46,7 +47,7 @@ export class DataFile extends Archive {
 export class MessageFile extends Archive {
     constructor(name:string, type:string, data: any[]) {
         super();
-        this.name = name;
+        this.name = name.replace(".","-");
         this.path = `${this.path}/${type}/${this.name}/data`;
         const dirPath = path.dirname(this.path);
         if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath, { recursive: true });
@@ -55,12 +56,13 @@ export class MessageFile extends Archive {
 }
 
 function getTimeInfo(): string {
-    const now = new Date(); 
+    const now = new Date();
+    const mili = now.getMilliseconds();
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
     const day = now.getDate();
     const month = now.getMonth() + 1; 
     const year = now.getFullYear();
-    return `${seconds}_${minutes}${hours}${day}${month}${year}`;
+    return `${day}-${month}-${year}_${hours}${minutes}${seconds}${mili}`;
 }
