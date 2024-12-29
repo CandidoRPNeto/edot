5a96556a42d24382e919fb412df5df20d94c38d0
CandidoRPNeto <candido.neto@ucsal.edu.br>
2024-12-29 04:25:58 -0300

    fix all crud basic uses and add log

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
-import {PgDataBase} from './db/DBQueue';
-
-console.log('Começamos os trabalhos');
-
-
+import { PgDataBase } from './db/DBQueue';
 const BANCO = new PgDataBase({
-    host : "localhost",
-    port : 5432,
-    user : "postgres",
-    database : "postgres",
-    password : "root"
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "postgres",
+    password: "root"
 });
 
 
-BANCO.insert('public."user"',{
-    id: "1",
-    nome: "Testinildo",
-    email: "t@t.t.com",
-    senha: "t123"
-});
+async function start() {
+    await BANCO.insert('public.users', {
+        nome: "Testinildo",
+        email: "t@t.t.com",
+        senha: "t123",
+        id: "1"
+    });
+    await BANCO.insert('public.users', {
+        nome: "Testinildo",
+        email: "t@t.t.com",
+        senha: "t123",
+        id: "1"
+    });
+
+    // await BANCO.select('public.users', { id: "1" }, []);
+
+    // await BANCO.update('public.users', { senha: "12345678" }, { id: "1" });
+
+    // await BANCO.select('public.users', { id: "1" }, []);
+
+    // await BANCO.delete('public.users', { id: "1" });
+}
+
+start();
