12fb4b9a4a6d810a264d663194a6760cf9da03b2
CandidoRPNeto <candido.neto@ucsal.edu.br>
2024-12-31 19:32:15 -0300

    implement reset function

--- src/db/SqlDataBase.ts
+++ src/db/SqlDataBase.ts
@@ -1,6 +1,7 @@
 import { Pool, QueryResult } from "pg";
 import { DataFile } from "../data/File";
 import { Messages } from "../data/Messages";
+import { SqlBuilds } from "./SqlBuilds";
 
 export interface PGConnectInfo{
     host: string;
@@ -41,7 +42,7 @@ export class PgDataBase implements SqlDataBase {
     }
 
     public async insert(table: string, object: {}) {
-        const insert = this.buildInsert(table, object);
+        const insert = SqlBuilds.insert(table, object);
         const message = Messages.new("INSERT",table);
         try {
             message.loading();
@@ -64,7 +65,7 @@ export class PgDataBase implements SqlDataBase {
             await client.query('BEGIN');
             for (const obj of objects) {
                 lastObj = obj;
-                const insert = this.buildInsert(table, obj);
+                const insert = SqlBuilds.insert(table, obj);
                 const response = await (await client.query(insert.query, insert.values)).rows;
                 result.push(response);
             }
@@ -78,20 +79,13 @@ export class PgDataBase implements SqlDataBase {
         await client.release();
         return new DataFile(table, result,"inserts");
     }
-
-    private buildInsert(table: string, object: {}) {
-        const columns = Object.keys(object).join(", ");
-        const values = Object.values(object);
-        let query = `INSERT INTO ${table} (${columns}) VALUES (${values.map((_, i) => `$${i + 1}`).join(", ")})`;
-        return {query: `${query} RETURNING *`, values: values};
-    }
     
     public async select(table: string, wheres: {}, fields: [] = []): Promise<DataFile> {
         let values = Object.values(wheres)? Object.values(wheres) : [];
         const message = Messages.new("SELECT",table);
         message.loading();
         try {
-            let data = await this.query(this.buildSelect(table, Object.keys(wheres), fields), values);
+            let data = await this.query(SqlBuilds.select(table, Object.keys(wheres), fields), values);
             message.success();
             return new DataFile(table, data.rows);
         } catch (error) { 
@@ -103,18 +97,15 @@ export class PgDataBase implements SqlDataBase {
     public async update(table: string, values: {}, wheres: {}) {
         let setKeys = Object.keys(values);
         let wheresKeys = Object.keys(wheres);
-        let query = `UPDATE ${table} SET`;
-        for  (let i = 0; i < setKeys.length; i++) { query = `${query} ${setKeys[i]} = $${i + 1},`; }
-        query = `${query.slice(0, -1)} WHERE ${wheresKeys[0]} = $${setKeys.length + 1}`;
-        for (let i = 1; i < wheresKeys.length; i++) { query = `${query} AND ${wheresKeys[i]} = $${i + setKeys.length + 1}`; }
+        const query = SqlBuilds.update(table,setKeys,wheresKeys);
         const data = Object.values(values).concat(Object.values(wheres));
-        const SELECTQUERY = this.buildSelect(table, wheresKeys);
+        const selectQuery = SqlBuilds.select(table, wheresKeys);
         const message = Messages.new("UPDATE",table);
         message.loading();
         try {
-            let oldOnes = await this.query(SELECTQUERY, Object.values(wheres));
+            let oldOnes = await this.query(selectQuery, Object.values(wheres));
             await this.query(query, data)
-            let newOnes = await this.query(SELECTQUERY, Object.values(wheres));
+            let newOnes = await this.query(selectQuery, Object.values(wheres));
             message.success();
             return new DataFile(table, {new: newOnes.rows, old: oldOnes.rows},"updates");
         } catch (error) {
@@ -126,12 +117,11 @@ export class PgDataBase implements SqlDataBase {
     public async delete(table: string, wheres: {}) {
         let keys = Object.keys(wheres);
         const values = Object.values(wheres);
-        let query = `DELETE FROM ${table} WHERE ${keys[0]} = $${1}`;
-        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
+        let query = SqlBuilds.delete(table,keys);
         const message = Messages.new("DELETE",table);
         message.loading();
         try {
-            let result = await this.query(this.buildSelect(table, keys), values);
+            let result = await this.query(SqlBuilds.select(table, keys), values);
             await this.query(query, values);
             message.success();
             return new DataFile(table, result.rows,"deletes");
@@ -141,11 +131,32 @@ export class PgDataBase implements SqlDataBase {
         }
     }
 
-    private buildSelect(table: string, keys: string[], fields: [] = []):string {
-        let columns = fields.length > 0 ? fields.join(",") : "*";
-        let query = `SELECT ${columns} FROM ${table} WHERE ${keys[0]} = $${1}`;
-        for (let i = 1; i < keys.length; i++) { query = `${query} AND ${keys[i]} = $${i + 1}`; }
-        return query;
+    public async reset(path:string) {
+        const data = DataFile.retrive(path);
+        const content = data.content();
+        switch (data.type) {
+            case "inserts":
+                const wheres = content.map((obj: { id: any; }) => ({ id: obj.id }));
+                let keys = Object.keys(wheres);
+                const values = Object.values(wheres);
+                let query = SqlBuilds.delete(data.name,keys);
+                await this.query(query, values);
+                break;
+            case "deletes":
+                //TODO
+                break;
+            case "updates":
+                content.old.forEach(async (obj: { id?: any; }) => {
+                    let setKeys = Object.keys(obj);
+                    const query = SqlBuilds.update(data.name,setKeys,["id"]);
+                    const sets = Object.values(obj).concat([obj.id]);
+                    await this.query(query, sets)
+                });
+                break;
+            default:
+                return;
+        }
+        data.destroy();
     }
     
     public async query(query: string, data: unknown[] = []) {
